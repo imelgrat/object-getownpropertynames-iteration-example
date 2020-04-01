973d55cc71bccf8fe2bd313256e32c8e268640cc
Iván Melgrati <imelgrat@users.noreply.github.com>
2020-03-31 23:16:41 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# object-getownpropertynames-iteration-example
-Created with CodeSandbox
+# Object.getOwnPropertyNames() example
+
+Unlike the previous methods, which only return enumerable properties, the **Object.getOwnPropertyNames()** method returns an array whose elements are strings corresponding to the enumerable and non-enumerable property names found in the object, without looking into the Prototype chain.
---
+++ src/index.js
@@ -0,0 +1,21 @@
+let car = { color: "blue", model: "Sandero", doors: 4 };
+const app = document.getElementById("app");
+const appEnum = document.getElementById("appEnum");
+// Add a property and set the enumerable flag to false
+Object.defineProperty(car, "nonEnumProp", {
+  value: "This will not show in Object.keys",
+  enumerable: false
+});
+
+// Create an array with the object's keys
+const properties = Object.getOwnPropertyNames(car);
+
+for (let key of properties) {
+  app.insertAdjacentHTML("beforeend", `${key}<br>`);
+}
+
+// Create an array with the object's enumerable values
+const enumerableProps = Object.keys(car);
+for (let key of enumerableProps) {
+  appEnum.insertAdjacentHTML("beforeend", `${key}<br>`);
+}
